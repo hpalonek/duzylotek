79447f29d3420116b27843274a6b015a1e376bef
hpalonek <hpalonek@gmail.com>
2016-11-09 22:41:31 +0100

    Create lotek module

---
+++ modul_lotek.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+def ustawienia():
+	while 1:
+		try:
+			ileliczb = int(raw_input('ile liczb chcesz losować? '))
+			maxliczb = int(raw_input('jaka ma być maksymalna losowana liczba? '))
+			ilelos = int(raw_input('ile razy chcesz typowac? '))
+			if ileliczb>maxliczb:
+				print 'bledne dane'
+				exit()
+			return (ileliczb, maxliczb, ilelos)
+		except:
+			print 'bledne dane'
+			continue
+
+	#print 'ustaliles ze chcesz losować', ileliczb, ' liczby ', 'a maksymalna liczba to ', maxliczb
+	
+
+def losowanie(ileliczb, maxliczb):
+	liczby = []
+	i = 0
+	while i < ileliczb:
+		liczba = random.randint(0, maxliczb)
+	
+		if liczby.count(liczba)==0:
+			liczby.append(liczba)
+			i=i+1
+	return liczby
+
+
+def typowanie(ileliczb, maxliczb):
+	print "Wytypuj", ileliczb, "z", maxliczb, "liczb:"
+
+	typy = set()
+	i = 0
+	while i < ileliczb:
+		try:
+			typ = int(raw_input('wytypuj liczbę '  + str(i + 1) + ': '))
+	
+		except ValueError:
+			print 'bledne dane'
+			continue
+		if 0<typ<maxliczb and typ not in typy:
+			typy.add(typ)
+			i = i + 1
+		else:
+			print 'cos nie tak jeszcz raz'
+			continue
+	return typy
+
+def wyniki(liczby, typy):
+	trafione = set(liczby) & typy
+
+	print 'wytypowales', typy
+	print 'trafione', trafione
+	print 'trafiles', len(trafione), 'razy'
+
+	#print 'wylosowano', liczby
+
+	return len(trafione)
+
